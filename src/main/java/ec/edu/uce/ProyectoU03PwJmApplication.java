b17032f184b24f0d8215c03a2ec0c18fc3aa2c58
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2023-02-02 15:46:50 -0500

    tarea11

---
+++ src/main/java/ec/edu/uce/ProyectoU03PwJmApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU03PwJmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU03PwJmApplication.class, args);
+	}
+
+}
